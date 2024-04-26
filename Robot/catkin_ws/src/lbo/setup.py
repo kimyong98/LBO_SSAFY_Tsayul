f25a7bb7cbccaafc2f9047b17ead271b7fdcf39d
김승배 <ksshc@snu.ac.kr>
2024-04-26 16:28:41 +0900

    [Feat] S10P31A408-110 : odometry 발행

---
+++ Robot/catkin_ws/src/lbo/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'lbo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='SSAFY',
+    maintainer_email='SSAFY@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'odom = lbo.odom:main',
+        ],
+    },
+)
